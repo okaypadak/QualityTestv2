62294cf718373aebec6e3f721b8285579402a938
Okay Padak <kypdk@proton.me>
2025-05-16 02:10:48 +0300

    güncel

--- src/main/java/dev/padak/backend/QualityTest/SecurityConfig.java
+++ src/main/java/dev/padak/backend/QualityTest/SecurityConfig.java
@@ -2,10 +2,10 @@ package dev.padak.backend.QualityTest;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-@EnableWebSecurity
-@Import({org.padak.JwtSecurityAutoConfiguration.class, org.padak.JwtService.class})
+@Import(org.padak.JwtSecurityAutoConfiguration.class)
 public class SecurityConfig {
+
+
 }
